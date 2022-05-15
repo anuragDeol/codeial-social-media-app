185ddcd6843d4ee90d0621faff2415e3ad8d1a22
Anurag Deol <anuragdeol2017@gmail.com>
2022-05-15 21:03:51 +0530

    add multiple routes and controllers

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,15 +1,3 @@
 module.exports.home = function(req, res){
     return res.end('<h1>Express is up for Codeial</h1>')
 }
-
-module.exports.actionName = function(req, res){
-    return res.end(`<h1 style="display:inline">This is what you(user) requested for- </h1> ${req.url}`);
-}
-
-module.exports.playground = function(req, res){
-    return res.end('<h1>Hello Ninja, welcome to the playground</h1>');
-}
-
-module.exports.hi = function(req, res){
-    return res.end('<h1>Hi!!</h1>');
-}
--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,7 @@ const homeController = require('../controllers/home_controller');
 console.log('router loaded');
 
 router.get('/', homeController.home);
-router.get('/action', homeController.actionName);
-router.get('/playground', homeController.playground);
-router.get('/hi', homeController.hi);
+router.use('/users', require('./users'));
 
 
 module.exports = router;    // all  http requests (get,post, etc..) will be handled by this module

ded7bb3e46d52fd0ab26748a366afb84848e2b02
Anurag Deol <anuragdeol2017@gmail.com>
2022-05-28 17:06:18 +0530

    sign up a user - completed

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: 'User Profile'
@@ -20,7 +22,29 @@ module.exports.signIn = function(req, res){
 
 // get the sign up data
 module.exports.create = function(req, res){
-    // todo later
+    // check whether pass and confirm pass are same or not
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    // if pass and confirm pass matches, then we check..
+    // finding the user id
+    User.findOne({email: req.body.email}, function(err, user){      // 'user' here is the document which is stored in our db, if it already exists, we get that document, else user is null
+        if(err){console.log('error in finding user in signing up'); return}
+        // console.log(`user: ${user}`);
+        
+        // if user id does not already exist, we create it in our db
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating user while signing up'); return}
+
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            // else we do not create it
+            return res.redirect('back');
+        }
+    });
 }
 
 // sign in and create a session for the user

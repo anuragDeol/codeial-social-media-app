026e94a8ef2ee6f1486a7521a77745b7bde90b40
Anurag Deol <anuragdeol2017@gmail.com>
2022-06-17 14:17:25 +0530

    displayed posts and the user who posted it

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,29 @@
+const Post = require('../models/post');
 // module.exports.home = function(req, res){
 //     return res.end('<h1>Express is up for Codeial</h1>')
 // }
 
 module.exports.home = function(req, res){
-    console.log(req.cookies);
+    // console.log(req.cookies);
     // res.cookie("user_id", 252);
-    return res.render('home', {
-        title: "Home"
-    });
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        if(err){
+            console.log(`error: ${err}`);
+            return;
+        }
+
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
+    })
 }
 

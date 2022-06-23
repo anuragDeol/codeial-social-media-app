0118256dd1233e51d0df5b3b959161ca48a2b244
Anurag Deol <anuragdeol2017@gmail.com>
2022-06-23 13:41:39 +0530

    1. show post form only when user is signed in, 2. created COMMENT schema

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const postsController = require('../controllers/posts_controller');
 
-router.post('/create', postsController.create);
+router.post('/create', passport.checkAuthentication, postsController.create);   // if user is not authenticated, this control won't be called
 
 module.exports = router;

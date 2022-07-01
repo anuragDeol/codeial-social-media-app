f0eee99ef91b23bcba1bc9589b8b36110e2b8a61
Anurag Deol <anuragdeol2017@gmail.com>
2022-07-01 09:55:49 +0530

    populated referenced schemas in post AND displayed comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -3,18 +3,23 @@ const Post = require('../models/post');
 
 
 module.exports.create = function(req, res){
-    // before creating comment, check if the post on which we want to comment, exists or not
+    // before creating comment, check if the post on which we want to comment, exists or not (anyone could have messed with the 'id' of our post via developer tools)
     Post.findById(req.body.post, function(err, post){
+        // error handling
+        if(err){`${err}: the post does not exist`};
+
         if(post){
-            // adding post id to the comment
+            // creating 'comment' document and adding into 'Comment' model alongwith, adding post 'id' to the comment
             Comment.create({
                 content: req.body.content,
-                post: req.body.post,
-                user: req.user._id
+                user: req.user._id,
+                post: req.body.post
             }, function(err, comment){
-                // HANDLE ERROR HERE...
+                // error handling
+                if(err){`${err}: unexpected error occured while commenting`};
+
                 // adding comment id to the post
-                post.comments.push(comment);    // mongodb finds the id and pushes in the array
+                post.comments.push(comment);    // mongodb finds the id of the 'comment' document which is taajaa taajaa created and pushes in the array
                 post.save();    // to save comment id in the database
             
                 res.redirect('/');
--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -14,7 +14,15 @@ module.exports.home = function(req, res){
     // });
 
     // populate the user of each post
-    Post.find({}).populate('user').exec(function(err, posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
         if(err){
             console.log(`error: ${err}`);
             return;

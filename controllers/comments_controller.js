79836266541fc86258ab0b2837ef588d1cf16afc
Anurag Deol <anuragdeol2017@gmail.com>
2022-07-05 11:59:37 +0530

    delete comment and post (only by the user who posted it)

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -27,3 +27,22 @@ module.exports.create = function(req, res){
         }
     });
 }
+
+module.exports.destroy = function(req, res){
+    Comment.findById(req.params.id, function(err, comment){
+        if(comment.user == req.user.id){
+            // before deleting comment, we need to fetch the id of the post to which this comment belongs to so that we can delete this comment from that post
+            
+            let postId = comment.post;
+
+            comment.remove();
+
+            Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}}, function(err, post){
+                return res.redirect('back');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
+}
--- routes/comments.js
+++ routes/comments.js
@@ -5,5 +5,6 @@ const passport = require('passport');
 const commentsController = require('../controllers/comments_controller');
 
 router.post('/create', passport.checkAuthentication, commentsController.create);   // if user is not authenticated, this control won't be called
+router.get('/destroy/:id', passport.checkAuthentication, commentsController.destroy);
 
 module.exports = router;

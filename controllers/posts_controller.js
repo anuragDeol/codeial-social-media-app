e81dd796d48e7397cbceaf43f795308afe167d80
Anurag Deol <anuragdeol2017@gmail.com>
2022-07-07 18:06:25 +0530

    styling - 7july22

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -18,7 +18,7 @@ module.exports.destroy = function(req, res){
     Post.findById(req.params.id, function(err, post){
         // checking if the user who's deleting the post, is the one who posted this post
         // .id means converting the object id into the string
-        if(post.user == req.user.id){
+        if(req.user.id == post.user){
             post.remove();
 
             Comment.deleteMany({post: req.params.id}, function(err){

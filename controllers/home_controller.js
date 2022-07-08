c36d83a7937621cc5137a8062f2eb16137ec64ab
Anurag Deol <anuragdeol2017@gmail.com>
2022-07-08 11:39:12 +0530

    converting HOME_CONTROLLER code to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,39 +4,30 @@ const User = require('../models/user');
 //     return res.end('<h1>Express is up for Codeial</h1>')
 // }
 
-module.exports.home = function(req, res){
-    // console.log(req.cookies);
-    // res.cookie("user_id", 252);
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts: posts
-    //     });
-    // });
+module.exports.home = async function(req, res){
 
-    // populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-
-        if(err){
-            console.log(`error: ${err}`);
-            return;
-        }
-
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts: posts,
-                all_users: users
-            });
+    try{
+        // populate the user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
         });
-    })
+
+        let users = await User.find({});
+
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts,
+            all_users: users
+        });
+    }catch(err){
+        console.log(`Error: ${err}`);
+        return;
+    }
+    
 }
 

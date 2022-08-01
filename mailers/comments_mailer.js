238be3e08e1846c9778939ac17dd91bf5105338c
Anurag Deol <anuragdeol2017@gmail.com>
2022-08-01 18:41:29 +0530

    send html/ejs content in mail(to the user who commented on a post) using nodemailer

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -7,13 +7,17 @@ const nodeMailer = require('../config/nodemailer');
 
 // exporting module - method 2
 exports.newComment = (comment) => {
-    console.log('inside newComment mailer');
+    // console.log('inside newComment mailer');
+    let htmlString = nodeMailer.renderTemplate({
+        // context
+        comment: comment
+    }, '/comments/new_comment.ejs');
 
     nodeMailer.transporter.sendMail({
         // from: 'codeial@gmail.com',   // the mail is sent from the email which is used to setup mailer in 'nodemailer.js' and is authorized by google
         to: comment.user.email,     // receiver's (the user who commented) email id
         subject: "New Comment Published",
-        html: "<h1>Yup, your comment is now published on Codeial :)</h1>"
+        html: htmlString
     }, (err, info) => {
         if(err){
             console.log("Error in sending mail", err);

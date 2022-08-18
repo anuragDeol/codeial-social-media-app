94a23807eb81ff3af188599af1f6a3c2362bea4c
Anurag Deol <anuragdeol2017@gmail.com>
2022-08-19 01:15:34 +0530

    like/unlike post asynchronously

---
+++ assets/js/toggle_likes.js
@@ -0,0 +1,43 @@
+
+class ToggleLike {
+    constructor(toggleElement) {
+        // 'toggleElement' is the href of <a> tag, on which the user clicked
+        this.toggler = toggleElement;
+        this.toggleLike();
+    }
+
+    toggleLike() {
+        $(this.toggler).click(function(e) {
+            // console.log('clicked- ' + this);
+            e.preventDefault();
+            let self = this;    // 'self' is assigned the <a> tag on which the user clicked
+            console.log(self);
+
+            // send request manually via AJAX
+            $.ajax({
+                type: 'POST',
+                url: $(self).attr('href')   // this fetches the href of the <a> tag
+            })
+            .done(function(data) {
+                let likesCount = parseInt($(self).attr('data-likes'));
+                console.log(likesCount);
+                if(data.data.deleted == true){
+                    // unlike the post - decrease likesCount
+                    likesCount -= 1;
+                }else{
+                    // like the post - increase likesCount
+                    likesCount += 1;
+                }
+
+                $(self).attr('data-likes', likesCount);
+                $(self).html(`${likesCount} Likes`);
+            })
+            .fail(function(err){
+                if(err) { console.log(err); return; }
+                console.log('error in completing the request');
+            });
+        });
+    }
+}
+
+
--- controllers/likes_controller.js
+++ controllers/likes_controller.js
@@ -4,17 +4,20 @@ const Comment = require('../models/comment');
 
 module.exports.toggleLike = async function(req, res){
     try{
-        // our request looks like -> likes/toggle/?id=randomstringhere&type=Post
-        let likeable;   // on what did we like - Post or Comment - (id of which) will be stored in likeable
+        // console.log(req.query.id);
+        // console.log(req.query.type);
+
+        // our request looks like -> likes/toggle/?id=post._id&type=Post
+        let likeableObject;   // on what did we like - Post or Comment - (the document of which) will be stored in likeableObject
         let deleted = false;    // like or unlike - i.e. if the user is liking for the first time, create a new 'like' document, else delete it
         
         // 1. find out what the user has liked
         if(req.query.type == 'Post'){
             // post is liked
-            likeable = await Post.findById('req.query.id').populate('likes');
+            likeableObject = await Post.findById(req.query.id).populate('likes');
         }else{
             // comment is liked
-            likeable = await Comment.findById('req.query.id').populate('likes');
+            likeableObject = await Comment.findById(req.query.id).populate('likes');
         }
 
         // 2. check if a like already exists
@@ -27,11 +30,15 @@ module.exports.toggleLike = async function(req, res){
 
         // 3. if a like already exist then delete it
         if(existingLike){
-            likeable.likes.pull(existingLike._id);
-            likeable.save();
+            //3a. if like object already exists, then first delete it from the post/comment to which it is referred to
+            likeableObject.likes.pull(existingLike._id);
+            likeableObject.save();
 
+            //3b. then delete that like object/document from the 'Like' model
             existingLike.remove();
             deleted = true;
+
+            console.log('** Unliked **');
         }else{
             // 4. else make a new like
             let newLike = await Like.create({
@@ -40,17 +47,28 @@ module.exports.toggleLike = async function(req, res){
                 onModel: req.query.type
             });
 
-            likeable.likes.push(newLike._id);
-            likeable.save();
+
+            likeableObject.likes.push(newLike._id);
+            likeableObject.save();
+
+            console.log('** Liked **');
         }
 
-        return res.json(200, {
+        // // deprecated syntax
+        // return res.json(200, {
+        //     message: "Request Successful",
+        //     data: {
+        //         deleted: deleted
+        //     }
+        // });
+
+        // newer syntax of above deprecated syntax
+        return res.status(200).json({
             message: "Request Successful",
             data: {
                 deleted: deleted
             }
         });
-
     }catch(err){
         // send back error msg as JSON object - AJAX request
         console.log(err);

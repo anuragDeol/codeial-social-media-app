bb37076aacbae5013ad18dadc4091878fda35f2a
Anurag Deol <anuragdeol2017@gmail.com>
2022-08-22 00:15:53 +0530

    chat engine complete (without db)

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -10,7 +10,7 @@ module.exports.chatSockets = function(socketServer){
     });
 
     io.on('connection', function(socket){
-        console.log('new connection received', socket.id);  // socket is object with lot of properties, one of them is 'id'
+        console.log('new connection received. socket.id:',socket.id);  // socket is object with lot of properties, one of them is 'id'
 
         socket.on('disconnect', function(){
             console.log('socket disconnected!');
@@ -38,34 +38,3 @@ module.exports.chatSockets = function(socketServer){
 
 
 
-// module.exports.chatSockets = function(socketServer) {
-//     let io = require('socket.io')(socketServer);
-
-
-//     io.sockets.on('connection', function(socket) {
-//         console.log('new connection received', socket.id);
-
-//         socket.on('disconnect', function() {
-//             console.log('socket disconnected');
-//         });
-
-
-//         // detects event sent by the client
-//         socket.on('join_room', function(data) {
-//             console.log('joining request received.', data);
-
-//             // join socket to the chatroom
-//             socket.join(data.chatroom);
-
-//             // emit an event here, it tells all the subscribers that a new subscriber has joined the chatroom
-//             io.in(data.chatroom).emit('user_joined', data);
-//         });
-
-
-//         socket.on('send_message', function(data) {
-//             io.in(data.chatroom).emit('receive_message', data);
-//         });
-
-
-//     });
-// }

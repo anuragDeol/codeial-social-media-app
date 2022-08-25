8c6e31e09c152aa25c597d26de683fea003acdab
Anurag Deol <anuragdeol2017@gmail.com>
2022-08-26 00:08:13 +0530

    styling-25aug22

---
+++ config/environment.js
@@ -0,0 +1,48 @@
+
+
+const development = {
+    name: 'development',
+    asset_path: './assets',
+    session_cookie_key: 'blahsomething',
+    db: 'codeial_development',
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',     // gmail's server
+        port: 587,
+        secure: false,
+        auth: {
+            // these credentials are used to authorize with google to use their mailer services - so these should be actual, correct and verified by google
+            user: 'anuragdeol2017@gmail.com',
+            pass: 'tfxuokixnzpzpwvy'    // app-generated password, from google, for our app (Codeial)
+        }
+    },
+    google_client_id: "278352446158-klp6n4to3bhbqglm7qm3453g5mi8gjmu.apps.googleusercontent.com",
+    google_client_secret: "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",
+    google_callback_url: "http://localhost:8000/users/auth/google/callback",
+    jwt_secret: 'codeial'
+}
+
+const production = {
+    name: 'production',
+    asset_path: process.env.CODEIAL_ASSET_PATH,
+    session_cookie_key: process.env.CODEIAL_SESSION_COOKIE_KEY,
+    db: process.env.CODEIAL_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',     // gmail's server
+        port: 587,
+        secure: false,
+        auth: {
+            // these credentials are used to authorize with google to use their mailer services - so these should be actual, correct and verified by google
+            user: process.env.CODEIAL_GMAIL_USERNAME,
+            pass: process.env.CODEIAL_GMAIL_PASSWORD    // app-generated password, from google, for our app (Codeial)
+        }
+    },
+    google_client_id: process.env.CODEIAL_GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
+    google_callback_url: process.env.CODEIAL_GOOGLE_CALLBACK_URL,
+    jwt_secret: process.env.CODEIAL_JWT_SECRET
+}
+
+
+module.exports = eval(process.env.CODEIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT);
